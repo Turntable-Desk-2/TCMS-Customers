8b4c36dee9cc97ba9c2d863a14229d6be3327e47
ankomahene <ankomaheneshadrack@gmail.com>
2019-11-20 19:11:52 +0000

    init

---
+++ src/test/java/io/turntabl/tcmsCustomers/TcmsCustomersApplicationTests.java
@@ -0,0 +1,13 @@
+package io.turntabl.tcmsCustomers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TcmsCustomersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
